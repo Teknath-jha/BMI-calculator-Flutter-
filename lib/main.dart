e7dc190a5807ce0402f8422f60685cc2e79bafb4
Teknath <jhateknath64@gmail.com>
2021-11-10 21:19:28 +0530

    BMI calculator app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bmi_calculator/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'BMI calculator',
+      debugShowCheckedModeBanner:  false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
